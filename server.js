a47a41b5308fe66f93ffc7b1700739d7d09039ae
teeshkeen <lxtishkin@gmail.com>
2024-11-10 16:37:12 +0500

    app listen ports fix

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Укажите путь к вашему фронтенду
 app.use(express.static(path.join(__dirname, '/build')));
 
 // Обработка всех маршрутов, чтобы они указывали на ваш index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
